fae2d3323bb407b47e6f9756cca91a96831dc3fd
Nils Breyer <mail@nilsbreyer.eu>
2017-07-16 18:10:16 +0200

    make map working in chrome?

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function includeMap() {
     link.rel = 'stylesheet';
     link.href = 'https://unpkg.com/leaflet@1.1.0/dist/leaflet.css';
     link.integrity = "sha512-wcw6ts8Anuw10Mzh9Ytw4pylW8+NAD4ch3lqm9lzAsTxg0GFeJgoAtxuCLREZSC5lUXdVyo/7yfsqFjQ4S+aKw==";
-    link.crossorigin="";
+    link.crossorigin="anonymous";
     document.head.appendChild(link);
 
     //include leaflet JS
@@ -68,7 +68,7 @@ function includeMap() {
     script.onload = loadMap;
     script.src = "https://unpkg.com/leaflet@1.1.0/dist/leaflet.js";
     script.integrity = "sha512-mNqn2Wg7tSToJhvHcqfzLMU6J4mkOImSPTxVZAdo+lcPlk+GhZmYgACEe0x35K7YzW1zJ7XyJV/TT1MrdXvMcA==";
-    script.crossorigin = "";
+    script.crossorigin = "anonymous";
     document.head.appendChild(script);
 }
 
