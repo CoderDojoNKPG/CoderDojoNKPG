6e2373c4f6c28e7f95161e59a5a7a367242e8932
Nils Breyer <mail@nilsbreyer.eu>
2017-07-16 18:19:53 +0200

    give up integrity for chrome

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function includeMap() {
 	var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = 'https://unpkg.com/leaflet@1.1.0/dist/leaflet.css';
-    link.integrity = "sha512-wcw6ts8Anuw10Mzh9Ytw4pylW8+NAD4ch3lqm9lzAsTxg0GFeJgoAtxuCLREZSC5lUXdVyo/7yfsqFjQ4S+aKw==";
+    //link.integrity = "sha512-wcw6ts8Anuw10Mzh9Ytw4pylW8+NAD4ch3lqm9lzAsTxg0GFeJgoAtxuCLREZSC5lUXdVyo/7yfsqFjQ4S+aKw==";
     link.crossorigin="anonymous";
     document.head.appendChild(link);
 
@@ -67,7 +67,7 @@ function includeMap() {
     var script = document.createElement('script');
     script.addEventListener("load", loadMap);
     script.src = "https://unpkg.com/leaflet@1.1.0/dist/leaflet.js";
-    script.integrity = "sha512-mNqn2Wg7tSToJhvHcqfzLMU6J4mkOImSPTxVZAdo+lcPlk+GhZmYgACEe0x35K7YzW1zJ7XyJV/TT1MrdXvMcA==";
+    //script.integrity = "sha512-mNqn2Wg7tSToJhvHcqfzLMU6J4mkOImSPTxVZAdo+lcPlk+GhZmYgACEe0x35K7YzW1zJ7XyJV/TT1MrdXvMcA==";
     script.crossorigin = "anonymous";
     document.head.appendChild(script);
 }
