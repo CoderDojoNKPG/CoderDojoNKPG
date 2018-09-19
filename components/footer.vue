dc026dc3a6f0222d082450f83ed186c5a30b1c8e
marcusnygren <marcusnygren.mn@gmail.com>
2018-09-19 21:47:43 +0200

    Created Nuxt structure

---
+++ components/footer.vue
@@ -0,0 +1,50 @@
+<template>
+	<div>
+		<p class="include-failed">Javascript krävs för att visa innehållet på sidan.</p>
+
+		<!--body will be included into all pages-->
+		<br/><br/><br/><br/>
+		<center>
+			<span class="comment">Vi ses på en dojo!</span><br/>
+			<img src="img/mascot/coderfox.png" class="mascot"/>
+		</center>
+
+
+		<footer>
+			<article>
+				<h3>Hitta hit</h3>
+	 			<p>Våra dojos hålls på Coffice &amp; InGenious (Laxholmstorget 3, 602 21 Norrköping). Specialevent kan hållas på annan plats.</p>
+	 			<p>
+	 				<div id="map" onload="loadMap()"></div>
+	 			</p>
+			</article>
+			<article>
+				<h3>Har du frågor?</h3>
+				<p>
+					<a href="mailto:info@coderdojonkpg.se" class="button"><span class="socicon socicon-mail"/> info@coderdojonkpg.se</a>
+				</p>
+				<h3>Vi finns även på</h3>
+					<a href="https://www.facebook.com/CoderDojoNkpg/" class="button"><span class="socicon socicon-facebook"></span></a>
+					<a href="https://twitter.com/CoderDojoNKPG" class="button"><span class="socicon socicon-twitter"></span></a>
+					<a href="https://www.instagram.com/coderdojonkpg/" class="button"><span class="socicon socicon-instagram"></span></a>
+			</article>
+			<article>
+				<h3>Föreningens styrelse</h3>
+				<p>
+					<a href="mailto:nils@coderdojonkpg.se"><span class="socicon socicon-mail"/></a> Nils Breyer, Ordförande<br/>
+					<a href="mailto:cecilia@coderdojonkpg.se"><span class="socicon socicon-mail"/></a> Cecilia Wyser, Kassör<br/>
+					<a href="mailto:kahin@coderdojonkpg.se"><span class="socicon socicon-mail"/></a> Kahin Akram, Spons- och marknadsföring<br/>
+					<a href="mailto:simon@coderdojonkpg.se"><span class="socicon socicon-mail"/></a> Simon Hansson, Coachansvarig<br/>
+					<a href="mailto:solveig@coderdojonkpg.se"><span class="socicon socicon-mail"/></a> Solveig Klingebiel, Grafiskt ansvarig<br/>
+				</p>
+			</article>
+			<article>
+				<p>Bakgrundsmönster: CC-BY-SA Steve Schoger www.heropatterns.com, Social icons: socicon.com</p>
+				<p>
+					© CoderDojo Norrköping 2018<br/>
+					org.nr 802492-5516
+				</p>
+			</article>
+		</footer>
+	</div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nav/>
+
+    <nuxt/>
+
+    <footer/>
+  </div>
+</template>
+
+<script>
+import Nav from '~/components/nav.vue'
+import Footer from '~/components/footer.vue'
+
+export default {
+  components: {
+    Nav,
+		Footer
+  }
+}
+</script>
